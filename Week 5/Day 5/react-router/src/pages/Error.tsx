bb688d36cb4d64da727860cffbb414b5b4085ab4
ItzAkmal <akmalm723@yahoo.com>
2024-11-17 15:13:42 +0500

    Bug fixes

--- Week 5/Day 5/react-router/src/pages/Error.tsx
+++ Week 5/Day 5/react-router/src/pages/Error.tsx
@@ -1,11 +1,14 @@
-import Header from "../components/Header"
-
+import { Link } from "react-router-dom"
 function Error() {
   return (
-    <main className="h-screen flex flex-col overflow-hidden">
-      <Header />
-      <div className="flex-1 flex justify-center items-center">
-        <h1 className="text-6xl font-bold text-stone-600">404! Page Not Found</h1>
+    <main className="h-screen flex flex-col">
+      <div className="flex-1 flex flex-col gap-8 justify-center items-center">
+        <span className="text-9xl font-bold text-stone-600">404!</span> 
+        <h1 className="text-5xl font-bold text-stone-600">Something’s not right.</h1>
+        <Link to={'/'}>
+        <button className="bg-stone-500 hover:bg-stone-700 text-white font-bold py-4 px-6 rounded-full mt-4">Back to Homepage</button>
+        </Link>
+    
       </div>
     </main>
   )
